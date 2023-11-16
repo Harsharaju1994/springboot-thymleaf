feb265dc9cc956a3c7d95ae17693aabe230d223a
Rajagopalreddy <raj.jobinfo97@gmail.com>
2023-11-16 12:02:11 +0530

    first program

---
+++ src/main/java/com/in60minutes/HomeController.java
@@ -0,0 +1,21 @@
+package com.in60minutes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("message","Hello-mama");
+        return "helloworld";
+    }
+
+    @GetMapping("/style")
+    public String style(){
+        return "add-css-js";
+    }
+}
